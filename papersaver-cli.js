c88817d7084613127575de337ffb2c71d1ce683e
Tyler Gaw <me@tylergaw.com>
2014-01-03 18:17:54 -0500

    Adding saver cli

---
+++ papersaver-cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var argv = require('optimist')
+    .usage('Creates a new Paper entry from -i, --image.\nUsage: $0')
+    .demand('i')
+    .alias('i', 'image')
+    .describe('i', 'The image from Paper')
+    .argv;
+
+var Papersaver = require('./paper-save'),
+    paperSaver = new Papersaver();
+
+paperSaver.save(argv.image);
