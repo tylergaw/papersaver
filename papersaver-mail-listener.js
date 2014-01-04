cf0a1ba44f8f5ea1bf372083cfd5495d6b154272
Tyler Gaw <me@tylergaw.com>
2014-01-03 19:10:19 -0500

    Get the creds info out of the script

---
+++ papersaver-mail-listener.js
@@ -0,0 +1,54 @@
+try {
+    var mailSettings = require('./mail-settings.json');
+}
+catch (e) {
+    console.log("You need to create the file 'mail-settings.json' based on 'mail-settings.sample.json'.");
+    return false;
+}
+
+var fs = require('fs'),
+    MailListener = require('mail-listener2'),
+    Papersaver = require('./papersaver'),
+    paperSaver = new Papersaver();
+
+var mailListener = new MailListener({
+    username: mailSettings.username,
+    password: mailSettings.password,
+    host: mailSettings.host,
+    port: 993,
+    tls: true,
+    tlsOptions: {
+        rejectUnauthorized: false
+    },
+    mailbox: 'INBOX',
+    markSeen: true
+});
+
+mailListener.start();
+
+mailListener.on('server:connected', function () {
+    console.log('Connected to IMAP server, listening for mail.');
+});
+
+mailListener.on('error', function (err) {
+    console.log(err);
+});
+
+mailListener.on('mail', function (mail) {
+    console.log('Mail received');
+
+    var att = mail.attachments;
+
+    if (att.length) {
+        var file = att[0].fileName;
+
+        fs.writeFile(file, att[0].content, function (err) {
+            if (err) throw err;
+
+            paperSaver.save(file);
+
+            // Cleanup
+            fs.unlink(file);
+        });
+    }
+});
