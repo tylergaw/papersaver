d8b3824c537b5ad7f523d936709cc23a1bf0c741
Tyler Gaw <me@tylergaw.com>
2014-01-04 01:47:16 -0500

    removing grunt deps

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,5 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        clean: {
-            build: [
-                'build'
-            ]
-        },
-        wintersmith: {
-            production: {
-                options: {
-                    config: './config-production.json'
-                }
-            },
-            preview: {
-                options: {
-                    action: 'preview',
-                    config: './config-preview.json'
-                }
-            }
-        }
-    });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-wintersmith');
+    });
 };
