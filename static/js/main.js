c65ff34e4dbc55f2a312a79b6729c6b717463b0b
Tyler Gaw <me@tylergaw.com>
2014-01-19 14:30:09 -0500

    Adding lightbox-style viewing of full sized papers

--- static/js/main.js
+++ static/js/main.js
@@ -18,7 +18,61 @@ require([
     'jquery',
     'underscore'
 ], function ($, _) {
+    var fullContainer = null;
+
+    function closeFullSizeContainer (e) {
+        if (e) {
+            e.preventDefault();
+        }
+
+        $('.papers .open').removeClass('open');
+        fullContainer.addClass('hidden');
+        history.pushState({}, '', '/papers/');
+    }
+
+    function createFullSizeContainer () {
+        var el = $('<div/>', {
+            'class': 'paper-container hidden',
+            'html': '<button class="close-btn">&times;</button>'
+        });
+
+        el.on('click', function (e) {
+            if (!$(e.target).is('img')) {
+                closeFullSizeContainer();
+            }
+        });
+
+        $('body').append(el);
+        return el;
+    }
+
+    function papersOnClick (e) {
+        e.preventDefault();
+
+        var el = $(e.currentTarget),
+            img = el.find('img').clone();
+
+        el.parents('.paper').addClass('open');
+        fullContainer.find('img').remove();
+        fullContainer.append(img);
+        fullContainer.removeClass('hidden');
+        history.pushState({}, '', el.attr('href'));
+    }
+
+    $('body').on('keyup', function (e) {
+        if (e.keyCode === 27) {
+            closeFullSizeContainer();
+        }
+    });
+
     $(function () {
-        console.log('help! my panties!');
+        if ($('.papers').length > 0) {
+            fullContainer = createFullSizeContainer();
+            $('.papers').on('click', 'a', papersOnClick);
+
+            window.onpopstate = function (e) {
+                closeFullSizeContainer();
+            };
+        }
     });
 });
